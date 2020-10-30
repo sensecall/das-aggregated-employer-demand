76fd1f6bf621569c78ae0d72f6bb83a50de298db
Dan Sensecall <dan@sensecall.co.uk>
2020-10-30 08:52:16 +0000

    set up routes

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,10 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.use(/\/aed-([0-9]+)-([0-9]+)/, (req, res, next) => {
+	req.version = req.originalUrl.split('/')[1]
+	req.session.data['prototypeVersion'] = req.version
+	
+	require(`./views/aed-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+})
