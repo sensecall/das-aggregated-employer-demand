d3efcaf8eae42d82597682761c3c2671d077a1a6
Dan Sensecall <dan@sensecall.co.uk>
2020-10-30 15:07:49 +0000

    first few pages of journey

--- app/views/aed-1-0/routes.js
+++ app/views/aed-1-0/routes.js
@@ -3,4 +3,20 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', (req, res, next) => {
+	res.redirect(`/${req.version}/start`)
+})
+
+router.post('/contacted-provider', (req, res, next) => {
+	res.redirect('provider-details')
+})
+
+router.post('/provider-details', (req, res, next) => {
+	res.redirect('standard-details')
+})
+
+router.post('/standard-details', (req, res, next) => {
+	res.redirect('contact-date')
+})
+
 module.exports = router
