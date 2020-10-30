8d3c092fbe6af676c33e9d8bcea28a81d828e77b
Dan Sensecall <dan@sensecall.co.uk>
2020-10-30 15:34:16 +0000

    more questions

--- app/views/aed-1-0/routes.js
+++ app/views/aed-1-0/routes.js
@@ -19,4 +19,20 @@ router.post('/standard-details', (req, res, next) => {
 	res.redirect('contact-date')
 })
 
+router.post('/contact-date', (req, res, next) => {
+	res.redirect('reason')
+})
+
+router.post('/reason', (req, res, next) => {
+	res.redirect('explainer')
+})
+
+router.post('/standard-flexibility', (req, res, next) => {
+	res.redirect('how-many-apprentices')
+})
+
+router.post('/how-many-apprentices', (req, res, next) => {
+	res.redirect('recruitment-required')
+})
+
 module.exports = router
