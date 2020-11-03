3cca6da023416726de7c4d379115f15065979438
Dan Sensecall <dan@sensecall.co.uk>
2020-11-03 16:32:04 +0000

    provider

--- app/views/provider-1-0/routes.js
+++ app/views/provider-1-0/routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 router.get('/', (req, res, next) => {
-	res.redirect(`/${req.version}/data`)
+	res.redirect(`/${req.version}/cohorts-list`)
 })
 
 router.post('/contacted-provider', (req, res, next) => {
